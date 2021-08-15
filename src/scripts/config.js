fdf9b94c680a042e50c30ac4c855c5e9035dd8a5
Alexander Vitchinov <vitalex92@yandex.ru>
2021-08-15 14:25:33 +0300

    feature/animation of first letters

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,3 +1,15 @@
-const aFormulaElement = document.querySelector('#aFormulaElement');
+const formulaSpan = document.querySelector('#formulaSpan');
+const aFormulaElement = document.querySelector('#aElement');
+const xFormulaElement = document.querySelector('#xElement');
+const parentNodeAElement = document.querySelector('.parentElem');
+const cardFormulaAnswer = document.querySelector('.card__formula-answer');
+const card = document.querySelector('.card');
 
-export { aFormulaElement };
+export {
+  aFormulaElement,
+  formulaSpan,
+  xFormulaElement,
+  parentNodeAElement,
+  cardFormulaAnswer,
+  card,
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,53 @@
 import '../styles.css';
+
+import {
+  aFormulaElement,
+  formulaSpan,
+  cardFormulaAnswer,
+  xFormulaElement,
+  parentNodeAElement,
+} from './config';
+
+// formulaSpan.textContent = aFormulaElement.textContent;
+
+const newAElement = aFormulaElement.cloneNode(true);
+const newXElement = xFormulaElement.cloneNode(true);
+formulaSpan.prepend(newAElement);
+formulaSpan.append(newXElement);
+
+newAElement.style.top = -99.5 + 'px';
+newAElement.style.left = -40 + 'px';
+newXElement.style.top = -99 + 'px';
+newXElement.style.left = 64 + 'px';
+
+function animate({ timing, draw, duration }) {
+  let start = performance.now();
+
+  requestAnimationFrame(function animate(time) {
+    let timeFraction = (time - start) / duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    let progress = timing(timeFraction);
+
+    draw(progress);
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+  });
+}
+
+animate({
+  duration: 400,
+  timing(timeFraction) {
+    return timeFraction;
+  },
+  draw(formulaSpan) {
+    newAElement.style.top = formulaSpan * 0;
+    newAElement.style.left = formulaSpan * 0;
+    newAElement.style.transition = 0.8 + 's';
+    newXElement.style.top = formulaSpan * 0 + 'px';
+    newXElement.style.left = formulaSpan * 0 + 'px';
+    newXElement.style.transition = 0.8 + 's';
+  },
+});
