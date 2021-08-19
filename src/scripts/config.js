c26194cefa3188ccc15f786f91a513874d0bf1c8
Alexander Vitchinov <vitalex92@yandex.ru>
2021-08-19 23:16:06 +0300

    function animation

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,15 +1,29 @@
-const formulaSpan = document.querySelector('#formulaSpan');
+const formulaFirstSpan = document.querySelector('#formulaFirstSpan');
 const aFormulaElement = document.querySelector('#aElement');
 const xFormulaElement = document.querySelector('#xElement');
+const yFormulaElement = document.querySelector('#yElement');
+const bFormulaElement = document.querySelector('#bElement');
+const formulaDots = document.querySelector('#cardFormulaDots');
+const formulaSecondSpan = document.querySelector('#formulaSecondSpan');
 const parentNodeAElement = document.querySelector('.parentElem');
 const cardFormulaAnswer = document.querySelector('.card__formula-answer');
 const card = document.querySelector('.card');
+const firstGroup = document.querySelectorAll('.card__formula-first');
+const secondGroup = document.querySelectorAll('.card__formula-second');
+const btnForward = document.querySelector('.card__button-container-forward');
 
 export {
   aFormulaElement,
-  formulaSpan,
+  formulaFirstSpan,
   xFormulaElement,
+  bFormulaElement,
+  yFormulaElement,
   parentNodeAElement,
   cardFormulaAnswer,
   card,
+  firstGroup,
+  secondGroup,
+  formulaDots,
+  formulaSecondSpan,
+  btnForward,
 };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,21 +2,30 @@ import '../styles.css';
 
 import {
   aFormulaElement,
-  formulaSpan,
-  cardFormulaAnswer,
+  formulaFirstSpan,
   xFormulaElement,
-  parentNodeAElement,
+  yFormulaElement,
+  bFormulaElement,
+  firstGroup,
+  secondGroup,
+  formulaDots,
+  formulaSecondSpan,
+  btnForward,
 } from './config';
 
-// formulaSpan.textContent = aFormulaElement.textContent;
-
+const firstGroupArray = Array.from(firstGroup);
 const newAElement = aFormulaElement.cloneNode(true);
 const newXElement = xFormulaElement.cloneNode(true);
-formulaSpan.prepend(newAElement);
-formulaSpan.append(newXElement);
+
+formulaFirstSpan.prepend(newAElement);
+formulaFirstSpan.append(newXElement);
+
+firstGroupArray.map((item) => {
+  item.style.opacity = 0;
+});
 
 newAElement.style.top = -99.5 + 'px';
-newAElement.style.left = -40 + 'px';
+newAElement.style.left = -51 + 'px';
 newXElement.style.top = -99 + 'px';
 newXElement.style.left = 64 + 'px';
 
@@ -37,17 +46,78 @@ function animate({ timing, draw, duration }) {
   });
 }
 
-animate({
+const newaElement = aFormulaElement.cloneNode(true);
+const newYElement = yFormulaElement.cloneNode(true);
+const SecondGroupArray = Array.from(secondGroup);
+
+formulaSecondSpan.prepend(newaElement);
+formulaSecondSpan.append(newYElement);
+formulaDots.remove();
+
+Array.from(
+  SecondGroupArray.map((item) => {
+    item.style.opacity = 0;
+  })
+);
+
+newaElement.style.top = -100 + 'px';
+newaElement.style.left = -182 + 'px';
+newYElement.style.top = -100 + 'px';
+newYElement.style.left = 12 + 'px';
+
+function animate1({ timing, draw, duration }) {
+  let start = performance.now();
+
+  requestAnimationFrame(function animate(time) {
+    let timeFraction = (time - start) / duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    let progress = timing(timeFraction);
+
+    draw(progress);
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+  });
+}
+
+animate1({
   duration: 400,
   timing(timeFraction) {
     return timeFraction;
   },
-  draw(formulaSpan) {
-    newAElement.style.top = formulaSpan * 0;
-    newAElement.style.left = formulaSpan * 0;
-    newAElement.style.transition = 0.8 + 's';
-    newXElement.style.top = formulaSpan * 0 + 'px';
-    newXElement.style.left = formulaSpan * 0 + 'px';
-    newXElement.style.transition = 0.8 + 's';
+  draw(formulaSecondSpan) {
+    newaElement.style.top = formulaSecondSpan * 0 + 'px';
+    newaElement.style.left = formulaSecondSpan * 0 + 'px';
+    newaElement.style.transition = 0.8 + 's';
+    newYElement.style.top = formulaSecondSpan * 0 + 'px';
+    newYElement.style.left = formulaSecondSpan * 0 + 'px';
+    newYElement.style.transition = 0.8 + 's';
+    SecondGroupArray.map((item) => {
+      item.style.opacity = 1;
+      item.style.transition = 0.3 + 's';
+    });
   },
+});
+
+btnForward.addEventListener('click', () => {
+  animate({
+    duration: 400,
+    timing(timeFraction) {
+      return timeFraction;
+    },
+    draw(formulaFirstSpan) {
+      newAElement.style.top = formulaFirstSpan * 0;
+      newAElement.style.left = formulaFirstSpan * 0;
+      newAElement.style.transition = 0.8 + 's';
+      newXElement.style.top = formulaFirstSpan * 0 + 'px';
+      newXElement.style.left = formulaFirstSpan * 0 + 'px';
+      newXElement.style.transition = 0.8 + 's';
+      firstGroupArray.map((item) => {
+        item.style.opacity = 1;
+        item.style.transition = 0.3 + 's';
+      });
+    },
+  });
 });
